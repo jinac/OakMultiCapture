f715a8a20c3d095b2a13add8eea4ad91cd5dc51a
jinac <jdevinacay@gmail.com>
2025-08-05 17:15:43 -0700

    Move things into functions

--- main.py
+++ main.py
@@ -13,6 +13,42 @@ def createPipeline(pipeline):
     output = camRgb.requestOutput((1280, 800), dai.ImgFrame.Type.NV12 ,dai.ImgResizeMode.CROP, 20).createOutputQueue()
     return pipeline, output
 
+def initStream(stack, deviceInfo):
+    pipeline = stack.enter_context(dai.Pipeline())
+    device = pipeline.getDefaultDevice()
+
+    print("===Connected to ", deviceInfo.getDeviceId())
+    mxId = device.getDeviceId()
+    cameras = device.getConnectedCameras()
+    usbSpeed = device.getUsbSpeed()
+    eepromData = device.readCalibration2().getEepromData()
+
+    print("   >>> Device ID:", mxId)
+    print("   >>> Num of cameras:", len(cameras))
+    if eepromData.boardName != "":
+        print("   >>> Board name:", eepromData.boardName)
+    if eepromData.productName != "":
+        print("   >>> Product name:", eepromData.productName)
+
+    pipeline, output = createPipeline(pipeline)
+    pipeline.start()
+
+    return pipeline, output
+
+def displayFrame(msg):
+        assert isinstance(msg, dai.ImgFrame)
+        frame = msg.getCvFrame()
+        seqNum = msg.getSequenceNum()
+        timestamp = msg.getTimestamp()
+        frame = cv2.putText(
+            frame,
+            str(timestamp),
+            (50,50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0))
+        cv2.imshow(f"video_device{i}", frame)
+
 def run():
     with contextlib.ExitStack() as stack:
         deviceInfos = dai.Device.getAllAvailableDevices()
@@ -21,46 +57,18 @@ def run():
         pipelines = []
 
         for deviceInfo in deviceInfos:
-            pipeline = stack.enter_context(dai.Pipeline())
-            device = pipeline.getDefaultDevice()
+            pipeline, output = initStream(stack, deviceInfo)
 
-            print("===Connected to ", deviceInfo.getDeviceId())
-            mxId = device.getDeviceId()
-            cameras = device.getConnectedCameras()
-            usbSpeed = device.getUsbSpeed()
-            eepromData = device.readCalibration2().getEepromData()
-
-            print("   >>> Device ID:", mxId)
-            print("   >>> Num of cameras:", len(cameras))
-            if eepromData.boardName != "":
-                print("   >>> Board name:", eepromData.boardName)
-            if eepromData.productName != "":
-                print("   >>> Product name:", eepromData.productName)
-
-            pipeline, output = createPipeline(pipeline)
-            pipeline.start()
             pipelines.append(pipeline)
-
             queues.append(output)
 
-        print(pipelines)
-        print(queues)
+        # print(pipelines)
+        # print(queues)
 
         while True:
             for i, stream in enumerate(queues):
                 videoIn = stream.get()
-                assert isinstance(videoIn, dai.ImgFrame)
-                frame = videoIn.getCvFrame()
-                seqNum = videoIn.getSequenceNum()
-                timestamp = videoIn.getTimestamp()
-                frame = cv2.putText(
-                    frame,
-                    str(timestamp),
-                    (50,50),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1,
-                    (0, 255, 0))
-                cv2.imshow(f"video_device{i}", frame)
+                displayFrame(videoIn)
             if cv2.waitKey(1) == ord('q'):
                 break
 
